2658b145c65f6aa332c43cd73fdf9042df8c1ec8
David <programmingdonebydavid@gmail.com>
2023-09-10 11:52:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 	/*
 	 * Were're using a printf to prints a text to the stdio
