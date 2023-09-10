4d86a87d7eb2c22bb95230a0bbf147ebc00bf718
David <programmingdonebydavid@gmail.com>
2023-09-10 11:47:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a function that prints out some text
+ * return - success 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
