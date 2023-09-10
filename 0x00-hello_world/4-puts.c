ee4dc89df6e1d8ad685904d155f28e7f67eb6589
David <programmingdonebydavid@gmail.com>
2023-09-10 10:57:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,9 @@
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts( "Programming is like building a multilingual puzzle\n" );
 	return(0);
+	/*
+	 * This prints out a text
+	*/
 }
