5d36453a76248941872ce93f19afc66460bf7e94
David <programmingdonebydavid>
2023-10-11 06:29:31 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - checks if a character is lower
+ * @c: the character constant to be check
+ *
+ * Return: 1 is character is lower 0 otherwise
+ */
+int _islower(int c)
+{
+	int i, l;
+
+	l = 'a';
+	while (a <= 'z')
+	{
+		if (c == a)
+			return (1);
+		l++;
+	}
+	return (0);
+
+}
