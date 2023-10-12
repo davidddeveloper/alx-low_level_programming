fd82a3cc15ec564dbfdf40d2bbacbb9bcc79e4fb
David <programmingdonebydavid>
2023-10-12 07:58:52 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: first integer to compute
+ * @b: second integer to compute
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,3 +13,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 int _abs(int);
 void times_table(void);
+int add(int, int);
