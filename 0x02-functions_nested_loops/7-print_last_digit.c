1127c3b62fddbe8d8d5cc97294cab563748cd994
David <programmingdonebydavid>
2023-10-12 02:47:10 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - get the last digit of a given number
+ * @n: number to be check
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int _islower(int c);
 int _isalpha(int c);
 int _isalpha2(int c);
 int print_sign(int n);
+int print_last_digit(int);
