58fb3bdcb63ff6297abce861e0fd336951dd695d
David <programmingdonebydavid>
2023-10-19 16:32:30 -0700

    C-More Functions and Nested Loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,17 @@
  */
 void print_number(int n)
 {
+	unsigned int nl;
+
+	nl = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		nl = -n;
 	}
 
-	if (n / 10 != 0)
-		print_number(n / 10);
+	if (nl / 10 != 0)
+		print_number(nl / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((nl % 10) + '0');
 }
