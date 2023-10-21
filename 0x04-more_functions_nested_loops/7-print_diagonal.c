ec6a39923ba74d7e762a8346dd33f83ad368139f
David <programmingdonebydavid>
2023-10-21 16:05:48 -0700

    C-More functions and nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,11 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
+		for (j = 0; j <= i; j++)
 			_putchar(' ');
-			j++;
-		}
+
 		_putchar('\\');
 		_putchar('\n');
-		i++;
 	}
 	if (n <= 0)
 		_putchar('\n');
