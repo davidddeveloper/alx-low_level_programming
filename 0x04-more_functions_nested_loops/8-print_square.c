83dd2ad6932a1dedc02306dd6b6df23b38e73b84
David <programmingdonebydavid>
2023-10-21 16:54:01 -0700

    C-More functions and nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,14 @@
 #include "main.h"
-
 /**
- * print_square - prints a square, followed by a new line;
- * @size: size of the square
+ * print_square - prints a square in terminal
+ * @size - A number that specifies the size of the square
  */
 void print_square(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
+	int i, j;
 
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			_putchar('#');
+	_putchar('\n');
 }
