656778bb5fd9d2b5f4b00e4d58a5043f1a4758b0
davidddeveloper <programmingdonebydavid@gmail.com>
2023-11-07 05:58:06 -0800

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 #include "main.h"
-struct dog dog_t;
-struct dog {
+
+/**
+ * struct dog - Details about a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
