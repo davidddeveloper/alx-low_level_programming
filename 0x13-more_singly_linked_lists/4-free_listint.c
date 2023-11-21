23382cd0859034127828178f006fbb29389a000c
davidddeveloper <programmingdonebydavid@gmail.com>
2023-11-21 13:20:54 -0800

    Created a readme file

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: start of the singled linked list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	free(head);
+}
