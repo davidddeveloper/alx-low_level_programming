9bdb462398c979b303a5e8f7841ec23f2bbfc455
davidddeveloper <programmingdonebydavid@gmail.com>
2023-11-21 13:59:24 -0800

    3-add_nodeint_end.c: change the conditions

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,14 +18,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if ((*head) == NULL)
 	{
 		*head = new_node;
-		return (new_node);
 	}
-	
-	temp = *head;
-	while (temp->next != NULL)
-		temp = temp->next;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
 
-	temp->next = new_node;
+		temp->next = new_node;
+	}
 
 	return (new_node);
 }
