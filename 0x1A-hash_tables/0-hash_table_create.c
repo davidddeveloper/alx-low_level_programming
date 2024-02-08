8d2b70d3990ff7dc7d126f79eade7531360e3103
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-08 08:50:01 -0800

    created a working hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table.
+ * @size: the size of the array
+ *
+ * Return: a pointer to the table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+
+	table->size = size;
+	/*table->array = malloc(sizeof(hash_node_t*) * size);*/
+
+	return (table);
+}
