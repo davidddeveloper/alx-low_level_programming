166ac4f54402df8d134e6722228f377269261dad
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-09 03:19:07 -0800

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: represents a structure hash table
+ * @key: is the key
+ * @value: the data
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+
+	/* Create the data */
+	hash_node_t *node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	hash_node_t *current_node;
+
+	/* handle if malloc failed */
+	if (node == NULL)
+		return (0);
+
+	node->key = (char *)key;
+	node->value = (char *)value;
+	node->next = NULL;
+
+	/* generates an index key */
+	index = key_index((unsigned char *)node->key, ht->size);
+
+	/* checks that the position of the table is empty */
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node; /* inserts the data to the table */
+		return (1);
+	}
+	/* handle collision */
+	current_node = ht->array[index];
+	if (current_node->key != node->key)
+	{
+		node->next = current_node;
+		current_node = node;
+		ht->array[index] = current_node;
+	}
+	return (1);
+}
