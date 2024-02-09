d63f8797c608505f0c003abaa30fe9787c0b065b
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-09 05:04:12 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,42 +5,32 @@
  * @ht: represents a structure hash table
  * @key: is the key
  * @value: the data
- *
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-
-	/* Create the data */
 	hash_node_t *node = (hash_node_t *) malloc(sizeof(hash_node_t));
 	hash_node_t *current_node;
 
-	/* if memory has not been allocated for the table */
-	if (ht == NULL)
+	if (ht == NULL) /* table has not been created */
 	{
 		free(node);
 		return (0);
 	}
-	/* handle if malloc failed */
-	if (node == NULL)
+	if (node == NULL) /* handle if malloc failed */
 	{
 		free(node);
 		return (0);
 	}
-
 	node->value = strdup(value);
 	node->key = strdup(key);
 	node->next = NULL;
-
 	/* generates an index key */
 	index = key_index((unsigned char *)node->key, ht->size);
-
-	/* checks that the position of the table is empty */
-	if (ht->array[index] == NULL)
+	if (ht->array[index] == NULL) /* checks if index is empty in table */
 	{
 		ht->array[index] = node; /* inserts the data to the table */
-		free(node->key);
 		return (1);
 	}
 	/* handle collision */
@@ -53,10 +43,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		/*free(node->key);
+		free(node->key);
 		free(node->value);
 		free(node);
-		free(current_node->value);*/
+		free(current_node->value);
 		current_node->value = (char *)value;
 	}
 	return (1);
