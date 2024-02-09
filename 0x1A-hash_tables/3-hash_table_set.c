a1398d43ee5f0e90eeedd628918029d91ce425b0
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-09 07:22:21 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,7 +45,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(node->value);
 		free(node);
-		free(current_node->value);
 		current_node->value = (char *)value;
 	}
 	return (1);
