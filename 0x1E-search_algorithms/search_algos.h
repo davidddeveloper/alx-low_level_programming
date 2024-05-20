2efc821636b3e67491ee9a6255650873829b3e44
davidddeveloper <programmingdonebydavid@gmail.com>
2024-05-20 07:36:03 -0700

    C - Search Algorithms - 0-linear-search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Function prototypes
+ */
+int linear_search(int *array, size_t size, int value);
+#endif /* SEARCH_ALGO_H */
